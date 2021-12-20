fa9298e0014202c12d9d51979b517df133d5e96c
Michal Kuklis <michal.kuklis@gmail.com>
2021-12-20 15:31:15 -0500

    Turn on lazy loading for ui modules

--- webpack/stripes-module-parser.js
+++ webpack/stripes-module-parser.js
@@ -111,10 +111,11 @@ class StripesModuleParser {
   // This will be modified to a webpack-compatible dynamic import when we implement code-splitting.
   parseStripesConfig(moduleName, packageJson) {
     const { stripes, description, version } = packageJson;
-
     const stripeConfig = _.omit(Object.assign({}, stripes, this.overrideConfig, {
       module: moduleName,
-      getModule: new Function([], `return require('${moduleName}').default;`), // eslint-disable-line no-new-func
+      getModule: new Function([], `
+        const { lazy } = require('react');
+        return lazy(() => import(/* webpackChunkName: "${moduleName}" */ '${moduleName}'));`),
       description,
       version,
     }), TOP_LEVEL_ONLY);
