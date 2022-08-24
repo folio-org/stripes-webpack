08e7e4a41d8f0195322dd571acabaa3afb06df96
Michal Kuklis <michal.kuklis@gmail.com>
2022-08-24 11:54:44 -0400

    Cleanup

--- webpack/transpile.js
+++ webpack/transpile.js
@@ -11,11 +11,20 @@ module.exports = function transpile(options = {}) {
 
     // TODO: allow for name customization
     const moduleTranspileConfigPath = path.join(process.cwd(), 'webpack.transpile.config.js');
+    const packagePath = path.join(process.cwd(), 'package.json');
 
     if (tryResolve(moduleTranspileConfigPath)) {
       const moduleTranspileConfig = require(moduleTranspileConfigPath);
       config.externals = { ...config.externals, ...moduleTranspileConfig.externals };
       config = { ...config, ...moduleTranspileConfig }
+    }
+
+    if (tryResolve(packagePath)) {
+      const packageJson = require(packagePath);
+      config.output.library = {
+        type: 'umd',
+        name: packageJson.name,
+      };
     }
 
     // Give the caller a chance to apply their own webpack overrides
