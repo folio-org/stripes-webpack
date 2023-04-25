6a06fb703b71de5ed2da0913142f4c3238c92922
Michal Kuklis <michal.kuklis@gmail.com>
2023-04-25 10:20:33 -0400

    Cleanup

--- consts.js
+++ consts.js
@@ -1,32 +1,3 @@
-// The list of the default externals
-// https://webpack.js.org/configuration/externals/
-const defaultExternals = [
-  '@folio/stripes',
-  '@folio/stripes-components',
-  '@folio/stripes-connect',
-  '@folio/stripes-core',
-  '@folio/stripes-util',
-  '@folio/stripes-form',
-  '@folio/stripes-final-form',
-  '@folio/stripes-logger',
-  '@folio/stripes-smart-components',
-  'final-form',
-  'final-form-arrays',
-  'moment',
-  'moment-timezone',
-  'react',
-  'react-dom',
-  'react-final-form',
-  'react-final-form-arrays',
-  'react-final-form-listeners',
-  'react-intl',
-  'react-query',
-  'react-redux',
-  'react-router',
-  'redux',
-  'stripes-config',
-];
-
 const singletons = [
   '@folio/stripes',
   '@folio/stripes-shared-context',
@@ -40,6 +11,5 @@ const singletons = [
 ];
 
 module.exports = {
-  defaultExternals,
   singletons,
 };
