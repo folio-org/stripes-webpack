467f39049a9203f4a759a8652b9f281ff629b388
Michal Kuklis <michal.kuklis@gmail.com>
2023-04-25 10:20:33 -0400

    Cleanup

--- consts.js
+++ consts.js
@@ -1,17 +1,14 @@
-// TODO: should these come from https://github.com/folio-org/stripes-core/blob/1d5d4f00a3756702e828856d4ef9349ceb9f1c08/package.json#L116-L129
-const singletons = {
-  '@folio/stripes': '^9.0.0',
-  '@folio/stripes-shared-context': '^1.0.0',
-  'react': '^18.2',
-  'react-dom': '^18.2',
-  'react-intl': '^6.4.4',
-  'react-query': '^3.39.3',
-  'react-redux': '^8.0.5',
-  'react-router': '^5.2.0',
-  'react-router-dom': '^5.2.0',
-  'redux-observable': '^1.2.0',
-  'rxjs': '^6.6.3'
-};
+const singletons = [
+  '@folio/stripes',
+  '@folio/stripes-shared-context',
+  'react',
+  'react-dom',
+  'react-router',
+  'react-router-dom',
+  'react-redux',
+  'react-intl',
+  'react-query',
+];
 
 module.exports = {
   singletons,
