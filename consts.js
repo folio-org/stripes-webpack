97ba1557e4597b3c355823315397426b31fdbb93
Michal Kuklis <michal.kuklis@gmail.com>
2023-04-27 14:43:49 -0400

    Add required version to shared singletons

--- consts.js
+++ consts.js
@@ -1,14 +1,16 @@
-const singletons = [
-  '@folio/stripes',
-  '@folio/stripes-shared-context',
-  'react',
-  'react-dom',
-  'react-router',
-  'react-router-dom',
-  'react-redux',
-  'react-intl',
-  'react-query',
-];
+// TODO: should these come from https://github.com/folio-org/stripes-core/blob/1d5d4f00a3756702e828856d4ef9349ceb9f1c08/package.json#L116-L129
+const singletons = {
+  '@folio/stripes': '^8.1.0',
+  '@folio/stripes-shared-context': '^1.0.0',
+  'react': '^17.0.2',
+  'react-dom': '^17.0.2',
+  'react-intl': '^5.7.0',
+  'react-redux': '^8.0.5',
+  'react-router': '^5.2.0',
+  'react-router-dom': '^5.2.0',
+  'redux-observable': '^1.2.0',
+  'rxjs': '^6.6.3'
+};
 
 module.exports = {
   singletons,
