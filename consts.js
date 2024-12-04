42cac160705fc322434105e233918953f755f396
Zak Burke <zburke@ebsco.com>
2024-12-04 14:54:20 -0500

    current versions

--- consts.js
+++ consts.js
@@ -1,12 +1,12 @@
 // TODO: should these come from https://github.com/folio-org/stripes-core/blob/1d5d4f00a3756702e828856d4ef9349ceb9f1c08/package.json#L116-L129
 const singletons = {
-  '@folio/stripes': '^8.1.0',
+  '@folio/stripes': '^9.3.0',
   '@folio/stripes-shared-context': '^1.0.0',
-  'react': '^17.0.2',
-  'react-dom': '^17.0.2',
-  'react-intl': '^5.7.0',
+  'react': '~18.2',
+  'react-dom': '~18.2',
+  'react-intl': '^6.8.0',
   'react-query': '^3.39.3',
-  'react-redux': '^8.0.5',
+  'react-redux': '^8.1',
   'react-router': '^5.2.0',
   'react-router-dom': '^5.2.0',
   'redux-observable': '^1.2.0',
