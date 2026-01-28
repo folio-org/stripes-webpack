f0b958a4d3d77e0eb042f4316ce04d2772b7ce71
John Coburn <jcoburn@ebsco.com>
2026-01-28 15:40:05 -0600

    add rxjs/operators to singletons list

--- consts.js
+++ consts.js
@@ -2,6 +2,13 @@
 // If an item is not in this list, modules will each load their own version of it.
 // This can be problematic for React Context if mutliple copies of the same context are loaded.
 
+// This list is best thought of as 'aliases' vs package-names. Whether or not the host
+// app provides the dependency depends on exactly *how the remote app imports it.
+// ex:
+//    ✓ 'import { Route } from 'react-router'  // covered by this list
+//    ✗ import Route from 'react-router/route' // not covered
+// deep imports should be marked with their offending app for future pruning of this list.
+// ui-modules should not have deep imports unless they absolutely have to.
 const singletons = {
   '@folio/stripes-components': '^13.1.0',
   '@folio/stripes-connect': '^10.0.1',
@@ -16,6 +23,7 @@ const singletons = {
   'react-router-dom': '^5.2.0',
   'redux-observable': '^1.2.0',
   'rxjs': '^6.6.3',
+  'rxjs/operators': '^6.6.3', // for eholdings usage
 };
 
 /** getHostAppSingletons
